f6aed8295480d019f93da16a38f9843c7d718310
ajayr2512 <ajayr99@gmail.com>
2016-06-13 23:18:49 -0400

    Register Initial

--- rs/regserver.go
+++ rs/regserver.go
@@ -5,20 +5,36 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 )
 
 type peerInfo struct {
-	ttl  int
-	flag bool
+	ttl      int
+	flag     bool
+	hostName string
+	port     int
 }
+
+var peerDict map[int]peerInfo
+var idChan chan int
 
 // Deciding on whether it is the same host joining
 // or a new host is based on hostName/port combo
 // TO DO: Add new msg join which enables re-joining
 // on peer control.
 func main() {
+
 	log.Println("Starting Registration Server")
+	// Starting cookie generator
+	go func() {
+		i := 1
+		for {
+			idChan <- i
+			i = i + 1
+		}
+	}()
+
 	defer func() {
 		log.Println("Exiting RS")
 	}()
@@ -52,13 +68,30 @@ func main() {
 }
 
 func processRequest(msg []byte, conn net.Conn) {
-	// FINISH UP
 	scanner := bufio.NewScanner(msg)
-	for scanner.Scan() {
-		s := strings.Split(scanner.Text(), ":")
-		switch s[0] {
-		case "REGISTER":
-			// Handle this
+	scanner.Scan()
+	msgType := scanner.Text()
+	switch msgType {
+	case "REGISTER":
+		cookie := <-idChan
+		for scanner.Scan() {
+			s := strings.Split(scanner.Text(), ":")
+			switch s[0] {
+			case "HOSTNAME":
+				peerInfo.hostName = s[1]
+			case "PORT":
+				peerInfo.port = strconv.Atoi(s[1])
+			}
 		}
+
+		peerInfo.ttl = 7200
+		peerInfo.flag = true
+		peerDict[cookie] = peerInfo
+
+		reply := []byte("STATUS:PASS\nCOOKIE:" + stconv.Itoa(cookie))
+		if _, err := conn.Write(reply); err != nil {
+			log.Println(err)
+		}
+
 	}
 }
